6edda5f932ea409f46ba6477ae2fa8bef19aaffc
fwolf <fwolf@gamomat.berlin>
2023-09-15 12:14:52 +0200

    feat(T-109): read next() unread event on subscriber

--- src/subscriber.spec.ts
+++ src/subscriber.spec.ts
@@ -5,8 +5,23 @@ describe('Subscriber', () => {
   // Noop Event used for testing.
   enum TestEvent { A, B, C }
 
-  it('should read events', () => {
-    console.time('foo')
+
+  it('should read the first unread event', () => {
+    const subscriber = new Subscriber();
+
+    subscriber.push(TestEvent.A);
+    subscriber.push(TestEvent.B);
+    subscriber.push(TestEvent.C);
+
+    // Call next twice to make sure that the unread message is "read".
+    subscriber.next();
+
+    const result = subscriber.next();
+
+    expect(result).toBe(TestEvent.B);
+  });
+
+  it('should read all unread events', () => {
     const subscriber = new Subscriber();
 
     subscriber.push(TestEvent.A);
